3dc86a81a496def25a5fe706f154782ef7d34440
P.T Nyamuda <ptnrlab@gmail.com>
2023-04-19 15:53:02 +0200

    nav-bar and testimonials created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CorouselComponent } from './corousel/corousel.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    NavBarComponent,
+    CorouselComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule
